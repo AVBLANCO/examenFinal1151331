9b522c1aefcd76977cd77939ac667c941fc3d597
victor Manuel Blanco <victormanuelbm@ufps.edu.co>
2020-06-18 08:06:36 -0500

    Controller y Service

--- src/main/java/com/ufps/controller/Controlador.java
+++ src/main/java/com/ufps/controller/Controlador.java
@@ -2,8 +2,48 @@ package com.ufps.controller;
 
 public class Controlador {
 
-	public Controlador() {
-		// TODO Auto-generated constructor stub
+	 @Autowired
+		private IUsuarioService service; 
+		
+	    @GetMapping("/listar")
+		public String listar (Model model) {
+			List<Usuario> usuario=service.listar();
+			model.addAttribute("personas",usuario);
+			return "index";
+		}
+	    
+	    @GetMapping("/")
+	   	public String listar2 (Model model) {
+	   		return "Login";
+	   	}
+	    @GetMapping("/registro")
+	   	public String registrar (Model model) {
+	   		return "Registro";
+	   	}
+	   
+	    @GetMapping("/new")
+	    public String agregar(Model model) {
+	    	model.addAttribute("usuario", new Usuario());
+	    	return "Form";
+	    }
+	    
+	    @PostMapping("/save")
+	    public String save(@Validated Usuario u,Model model ) {
+	    	service.save(u);
+	    	return "redirect:/listar";
+	    }
+	    
+	    @GetMapping("/editar/{id}")
+	    public String editar(@PathVariable int id, Model model) {
+	    	Optional<Usuario> usuario=service.listarId(id);
+	    	model.addAttribute("usuario",usuario);
+	    	return "Form";
+	    }
+	    @GetMapping("/eliminar/{id}")
+	    public String delete(Model model, @PathVariable int id) {
+	    	service.delete(id);
+	    	return "redirect:/listar";
+	    }
+	    
+	    
 	}
-
-}
---
+++ src/main/java/com/ufps/interfaces/IPersona.java
@@ -0,0 +1,5 @@
+package com.ufps.interfaces;
+
+public interface IPersona {
+
+}
