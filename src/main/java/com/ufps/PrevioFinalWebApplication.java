5772d51d57da7e238bf5ae164e92e1a4d16f6c51
victor Manuel Blanco <victormanuelbm@ufps.edu.co>
2020-06-18 06:18:12 -0500

    Estructura

---
+++ src/main/java/com/ufps/PrevioFinalWebApplication.java
@@ -0,0 +1,13 @@
+package com.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevioFinalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevioFinalWebApplication.class, args);
+	}
+
+}
